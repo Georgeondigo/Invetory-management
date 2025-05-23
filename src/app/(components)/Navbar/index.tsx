fcbd59723a14d7b314895821301104fe4631e2e7
Georgeondigo <Georgeondigo8@mail.com>
2025-05-23 16:11:58 +0300

    Navbar section

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { Bell, Menu, Sun } from 'lucide-react'
+import { Bell, Menu, Settings, Sun } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -12,7 +13,7 @@ const Navbar = () => {
             <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-bg-blue-100' onClick={() => {}}>
                 <Menu className='w-4 h-4'/>
             </button>
-        </div>
+        
         <div className='relative'>
             <input type='search' placeholder='Start type to search groups & products' 
             className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-500'/>
@@ -20,6 +21,7 @@ const Navbar = () => {
             <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
               <Bell className=' test-gray-500 size={20}'/>
             </div>
+        </div>
         </div>
         {/*RIGHT SIDE*/}
         <div className='flex justify-between items-center gap-5'>
@@ -31,8 +33,19 @@ const Navbar = () => {
             </div>
             <div className='relative'>
               <Bell className='cursor-pointer text-gray-500' size={24}/>
+              <span className='absolute -top-2 -right-2  inline-flex justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full'>3</span>
+            </div>
+            <hr className='w-0 h-7 border-solid border-l border-gray-300 mx-3'/>
+            <div className='flex items-center gap-3 cursor-pointer'>
+              <div className='w-9 h-9'>
+                image
+              </div>
+              <span className='font-semibold'>Ondigo</span>
             </div>
           </div>
+          <Link href='/settings'>
+             <Settings className='cursor-pointer text-gray-500' size={24}/>
+          </Link>
         </div>
     </div>
   )
