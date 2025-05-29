84d2d8f3789decde3478ac8cb484174606cb937f
Georgeondigo <Georgeondigo8@mail.com>
2025-05-29 21:06:04 +0300

    settings UI update

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,45 +1,45 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import Header from '../(components)/Header';
+import React, { useState } from "react";
+import Header from "../(components)/Header";
 
 type UserSetting = {
-    label: string;
-    value: string | boolean;
-    type : "text" | "toggle";
-}
+  label: string;
+  value: string | boolean;
+  type: "text" | "toggle";
+};
 
-const mockSettings: UserSetting[] =[
-    {label: "Username",value: "ondigo", type: "text"},
-    {label: "Email",value: "ondigo@gmail.com", type: "text"},
-    {label: "Notification",value: true , type: "toggle"},
-    {label: "Dark Mode",value: false, type: "toggle"},
-    {label: "Language",value: "English", type: "text"}
-]
+const mockSettings: UserSetting[] = [
+  { label: "Username", value: "ondigo", type: "text" },
+  { label: "Email", value: "ondigo@gmail.com", type: "text" },
+  { label: "Notification", value: true, type: "toggle" },
+  { label: "Dark Mode", value: false, type: "toggle" },
+  { label: "Language", value: "English", type: "text" },
+];
 
-const  Settings = () => {
-    const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
+const Settings = () => {
+  const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
 
-    const handleToggleChange = (index: number) => {
-        const settingsCopy= [...userSettings];
-        settingsCopy[index].value = !settingsCopy[index].value as boolean;
-        setUserSettings(settingsCopy);
-    }
+  const handleToggleChange = (index: number) => {
+    const settingsCopy = [...userSettings];
+    settingsCopy[index].value = !settingsCopy[index].value as boolean;
+    setUserSettings(settingsCopy);
+  };
   return (
-    <div className='w-full'> 
-    <Header name='User Settings'/>
-    <div className="overflow-x-auto mt-5 shadow-md">
-        <table className='min-w-full bg-white rounded-lg'>
-            <thead className='bg-gray-800  text-white'>
-                <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Settings
-                </th>
-                <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-                    Value
-                </th>
-            </thead>
-            <tbody >
-                {userSettings.map((setting, index) => (
+    <div className="w-full ">
+      <Header name="User Settings" />
+      <div className="overflow-x-auto mt-5 shadow-md rounded-lg">
+        <table className="min-w-full bg-white rounded-lg">
+          <thead className="bg-gray-800  text-white">
+            <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+              Settings
+            </th>
+            <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+              Value
+            </th>
+          </thead>
+          <tbody>
+            {userSettings.map((setting, index) => (
               <tr className="hover:bg-blue-50" key={setting.label}>
                 <td className="py-2 px-4 ">{setting.label}</td>
                 <td className="py-2 px-4">
@@ -74,11 +74,11 @@ const  Settings = () => {
                 </td>
               </tr>
             ))}
-            </tbody>
+          </tbody>
         </table>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default  Settings
+export default Settings;
